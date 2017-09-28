f6f1decd0bafd33bd514b4548584a07916fcd3d0
Ryan Hemphill <ryanhemphill.email@gmail.com>
2017-09-28 17:58:32 -0400

    Focus Toggle Feature added
    Toggle Feature allows turning on/off, fixed attr “checked” on form

--- js/testing.js
+++ js/testing.js
@@ -14,6 +14,8 @@ $(document).ready(function() {
 
 function setupA11ySettingFunctions() {
 
+
+
   // font-family
   var fontFamily_radioButtons = $('[name="font-family"]');
   fontFamily_radioButtons.on('click', function() {
@@ -83,6 +85,35 @@ function setupA11ySettingFunctions() {
     }
   });
 
+  var focusToggleButton = $('#focus-toggle-button');
+  focusToggleButton.on('click', function() {
+    var self = $(this), 
+        selfToggleState = self.attr('aria-pressed'),
+        targetSample = $('.dys'),
+        targetNum = targetSample.attr('data-focus-size');
+    if(selfToggleState == 'false') {
+      targetSample
+        .addClass('dys-focus')
+        .find('.active').removeClass('active');
+      targetSample
+        .find('.line,.sentence')
+          .each(function(index) {
+            if(index < targetNum) {
+              $(this).addClass('active');
+            } else { return false; }
+          });
+      self
+        .text('Disable Focus Mode')
+        .attr('aria-pressed', 'true');
+    } 
+    else if(selfToggleState == 'true') {
+      targetSample
+        .removeClass('dys-focus');
+      self
+        .text('Apply Focus Mode')
+        .attr('aria-pressed', 'false');
+    }
+  });
 
   var focusControl_radioButtons = $('[name="focus"]');
   focusControl_radioButtons.on('click', function() {
@@ -93,10 +124,14 @@ function setupA11ySettingFunctions() {
       focusThis(sampleArea, false);
       sampleArea.removeClass('dys-focus');
       $('#line-style-gradient').removeAttr('disabled');
+      focusToggleButton.attr('aria-hidden', 'true');
+    } else {
+      focusToggleButton.attr('aria-hidden', 'false');
     }
     if(thisButton.attr('id') == 'focus-1-line') {
       focusThis(sampleArea, { type: 'lines', size: 1 });
       $('#line-style-gradient').removeAttr('disabled');
+      $('#line-style-gradient').removeAttr('aria-disabled');
     }
     if(thisButton.attr('id') == 'focus-3-lines') {
       focusThis(sampleArea, { type: 'lines', size: 3 });
@@ -111,4 +146,6 @@ function setupA11ySettingFunctions() {
       $('#line-style-gradient').attr('disabled', '');
     }
   });
+
+
 }
